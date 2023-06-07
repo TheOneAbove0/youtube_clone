a7c2677aa1674e1428f57dc4af76e2ba7d676cde
TheOneAbove0 <manishthapaliya8888@gmail.com>
2023-06-07 17:16:32 +0545

    some change

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
-import {Navbar, ChannelDetails, Feed, VideoDetails, SearchFeed} from './components'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import {
+  Navbar,
+  ChannelDetails,
+  Feed,
+  VideoDetails,
+  SearchFeed,
+} from "./components";
 
 function App() {
   return (
-   <BrowserRouter>
-   <Navbar />
-   <Routes>
-    <Route path='/' element={<Feed />} />
-    <Route path='/video/:id' element={<VideoDetails />} />
-    <Route path='/search/:searchTerm' element={<SearchFeed />} />
-    <Route path='/channel/:id' element={<ChannelDetails />} />
-
-    
-   </Routes>
-   
-   </BrowserRouter>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetails />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+        <Route path="/channel/:id" element={<ChannelDetails />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
